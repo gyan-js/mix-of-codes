ce72aa717aba0679ad1aa7039e49b6a1a73ede95
Gyan <gyan0304@gmail.com>
2023-02-18 08:40:09 -0500

    perf: Updated navigation routes in the dropdown 🚀

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { Disclosure, Menu } from '@headlessui/react'
-
+import { useNavigate } from 'react-router-dom'
 import { Popover, Transition } from '@headlessui/react'
 import { BsMoonStarsFill, BsCloudSunFill } from 'react-icons/bs'
 import { BiTerminal } from 'react-icons/bi'
 
 import { AiFillCaretDown } from 'react-icons/ai'
+
+import navigation_routes from '../context/navigation'
 
 function Navbar() {
   const [theme, setTheme] = useState()
@@ -20,27 +22,30 @@ function Navbar() {
   }, [])
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if(storedTheme){
-      setTheme(storedTheme);
-    }
-  }, [])
-  
 
-  useEffect(() => {
-      localStorage.setItem("theme", theme);
     if (theme === "dark") {
       document.documentElement.classList.add("dark")
     }
     else {
       document.documentElement.classList.remove("dark")
     }
-    
+    localStorage.setItem("theme", theme);
   }, [theme])
+
+  useEffect(() => {
+    var theme_stored = localStorage.getItem("theme")
+
+    if (theme_stored) {
+      setTheme(theme_stored);
+    }
+  }, [])
+
 
   const themeSwitch = () => {
     setTheme(theme === "dark" ? "light" : "dark")
   }
+
+  const navigate = useNavigate()
 
   return (
     <Popover as="nav" className=" sticky top-0 bg-[#080d16] ">
@@ -60,9 +65,10 @@ function Navbar() {
                   <>
                     <a
                       //key={item.name}
-                      href="#"
+                        
                       className='font-bold text-indigo-600 dark:text-white 
                           px-3 py-2 rounded-md  text-base flex flex-row text-lg'
+                          onClick={() => navigate('/')}
 
                     //aria-current={current ? 'page' : undefined}
                     >
@@ -73,12 +79,38 @@ function Navbar() {
                       <div
                         className=" bg-transparent text-gray-700 font-semibold py-2 px-8 rounded inline-flex items-center"
                       >
-                        <span className='text-indigo-600 dark:text-white font-bold mx-1  text-base md:text-base' >Posts</span>
+                        <button
+                          class="flex items-center hover:text-indigo-600 text-gray-800 dark:text-gray-50 mx-6 cursor-pointer"
+                        >
+                          <span class="mr-1 font-bold ">Dropdown</span>
+                          <AiFillCaretDown />
+                        </button>
+                        <ul className=" dark:bg-black rounded-br-8 ml-[-50px] mt-48 dropdown-menu absolute group-hover:block hidden text-gray-700 pt-1 bg-white dark:bg-dark w-40  rounded-xl left-1/3">
+                          <li class="">
+                            <a
+                              class="hover:bg-gray-300 rounded-t dark:bg-black text-gray-800 dark:text-gray-50 py-2 px-4 block whitespace-no-wrap"
+                              onClick={() => navigate(`/topics/${navigation_routes[0]}`)}
+                            >Git</a
+                            >
+                          </li>
+                          <li class="">
+                            <a
+                              class="dark:bg-black dark:text-gray-50 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap"
+                              
+                              onClick={() => navigate(`/topics/${navigation_routes[1]}`)}
+                            >Javascript</a
+                            >
+                          </li>
+                          <li class="">
+                            <a
+                              class="rounded-b dark:bg-black dark:text-gray-50 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap"
+                              onClick={() => navigate(`/topics/${navigation_routes[2]}`)}
+                            >Three is the magic number 
+                            </a>
+                          </li>
 
-                        <AiFillCaretDown className='text-indigo-700 dark:text-white'/>
-                        
 
-
+                        </ul>
                       </div>
 
                     </div>
@@ -93,9 +125,9 @@ function Navbar() {
               <div className=''>
                 <input type="checkbox" className="checkbox" id="checkbox" onChange={themeSwitch} />
                 <label for="checkbox" class="label">
-                <BsCloudSunFill className='text-indigo-700 dark:text-white ' />
-                <BsMoonStarsFill className='text-indigo-700 dark:text-white' />
-                  
+                  <BsCloudSunFill className='text-indigo-700 dark:text-white ' />
+                  <BsMoonStarsFill className='text-indigo-700 dark:text-white' />
+
                   <div class='ball bg-indigo-700 dark:bg-white' />
                 </label>
               </div>
