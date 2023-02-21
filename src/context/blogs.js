15ea62a6444e3fe49cf5cdef540970263bf5af68
Gyan <gyan0304@gmail.com>
2023-02-21 04:10:00 -0500

    perf: Updated the object 🚀

--- src/context/blogs.js
+++ src/context/blogs.js
@@ -3,25 +3,62 @@ import { DiReact } from 'react-icons/di'
 import { SiJavascript } from 'react-icons/si'
 
 const blogs = [
-  { 
-    Tag: <FaGitAlt size={25} />, 
-  title: 'Lorem ipsum dolor sit', 
-  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!' },
-  { 
-    Tag: <FaPython size={25} />, 
-  title: 'Lorem ipsum dolor sit', 
-  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!' },
-  { Tag: <SiJavascript size={25} />, 
-  title: 'Lorem ipsum dolor sit', 
-  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!' },
-  { Tag: <DiReact size={25} />, 
-  title: 'Lorem ipsum dolor sit', 
-  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!' },
-  { Tag: <DiReact size={25} />, 
-  title: 'Lorem ipsum dolor sit', 
-  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!' },
-
-
+  {
+    key: 'Git',
+    Tag: <FaGitAlt size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  
+  {
+    key: 'Javascript',
+    Tag: <SiJavascript size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  {
+    key: 'Javascript',
+    Tag: <SiJavascript size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  {
+    key: 'Javascript',
+    Tag: <SiJavascript size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  {
+    key: 'React',
+    Tag: <DiReact size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  {
+    key: 'React',
+    Tag: <DiReact size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  {
+    key: 'Git',
+    Tag: <FaGitAlt size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  
+  {
+    key: 'Javascript',
+    Tag: <SiJavascript size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
+  {
+    key: 'React',
+    Tag: <DiReact size={25} />,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium incidunt quos quidem quo eum iste eos iure voluptatibus perspiciatis temporibus!'
+  },
 ]
 
 export default blogs;
