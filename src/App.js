57001c46b1f4e563728f65471f61af71743d7988
Gyan <gyan0304@gmail.com>
2023-02-21 04:12:22 -0500

    perf: Implemented the Router 🚀

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Giscus from '@giscus/react';
-import {Navbar, Comment, Home} from './components'
+import { Navbar, Comment, Home, Topics } from './components'
+import {TGit, TJavascript, TReact} from './pages'
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/topics/git' element={<TGit />} />
+          <Route path='/topics/javascript' element={<TJavascript />} />
+          <Route path='/topics/reactjs' element={<TReact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
